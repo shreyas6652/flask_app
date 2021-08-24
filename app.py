2c89515592908d3829f30d54cbf1a0c43fe12d89
Shreyas Torgalmath <37019043+shreyas6652@users.noreply.github.com>
2021-08-24 16:32:32 +0530

    Form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
@@ -11,39 +11,38 @@ db=SQLAlchemy(app)
 
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    desc = db.Column(db.String(500), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    Name = db.Column(db.String(200), nullable=False)
+    Age = db.Column(db.String(500), nullable=False)
+    Mobile = db.Column(db.String(500), nullable=False)
 
     def __repr__(self) -> str:
-        return f"{self.sno} - {self.title}"
+        return f"{self.sno} - {self.Name}"
 
 
 @app.route('/',methods=['GET','POST'])
 def hello_world():
     if request.method=="POST":
-        title=request.form['title']
-        desc=request.form['desc']
-        todo=Todo(title=title,desc=desc)
+        Name=request.form['Name']
+        Age=request.form['Age']
+        Mobile=request.form['Mobile']
+        todo=Todo(Name=Name,Age=Age,Mobile=Mobile)
         db.session.add(todo)
         db.session.commit()
     allTodo=Todo.query.all()
     return render_template('index.html',allTodo=allTodo)
 
-@app.route('/show')
-def products():
-    allTodo = Todo.query.all()
-    print(allTodo)
-    return 'this is products page'
+
 
 @app.route('/update/<int:sno>',methods=['GET','POST'])
 def update(sno):
     if request.method=="POST":
-        title=request.form['title']
-        desc=request.form['desc']
+        Name=request.form['Name']
+        Age=request.form['Age']
+        Mobile=request.form['Mobile']
         todo = Todo.query.filter_by(sno=sno).first()
-        todo.title=title
-        todo.desc=desc
+        todo.Name=Name
+        todo.Age=Age
+        todo.Mobile=Mobile
         db.session.commit()
         return redirect("/")
     todo = Todo.query.filter_by(sno=sno).first()
