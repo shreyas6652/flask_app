53044924007ad93878c5306a904c16c3e71e3719
Shreyas Torgalmath <37019043+shreyas6652@users.noreply.github.com>
2021-08-25 13:00:32 +0530

    u

--- app.py
+++ app.py
@@ -1,59 +1,83 @@
-from flask import Flask,render_template,request,redirect,jsonify
+from flask import Flask,jsonify
+from flask_restful import Api,Resource,fields, marshal_with,reqparse
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app=Flask(__name__)
+api=Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 db = SQLAlchemy(app)
 
-db=SQLAlchemy(app)
-
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.String(200), nullable=False)
-    Age = db.Column(db.String(500), nullable=False)
-    Mobile = db.Column(db.String(500), nullable=False)
+    Name = db.Column(db.String(200), nullable=True)
+    Age = db.Column(db.String(500), nullable=True)
+    Mobile = db.Column(db.String(500), nullable=True)
 
-    def __repr__(self) -> str:
-        return f"{self.sno} - {self.Name}"
+    def __ref__(self):
+        return f"Todo(Name= {Name} ,Age= {Age} ,Mobile= {Mobile})"
 
+resource_fields={
+    'sno':fields.Integer,
+    'Name':fields.String,
+    'Age':fields.Integer,
+    'Mobile':fields.String
+}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("Name", type=str, required=True)
+video_put_args.add_argument("Age", type=str, required=True)
+video_put_args.add_argument("Mobile", type=str,  required=True)
 
-@app.route('/',methods=['GET','POST'])
-def hello_world():
-    if request.method=="POST":
-        Name=request.form['Name']
-        Age=request.form['Age']
-        Mobile=request.form['Mobile']
+class GetTable(Resource):
+    @marshal_with(resource_fields)
+    def get(self):
+        result=Todo.query.all()
+        jsonobj=[]
+        for itr in result:
+            jsonitr={
+                "sno":itr.sno,
+                "Name":itr.Name,
+                "Age":itr.Age,
+                "Mobile":itr.Mobile
+            }
+            jsonobj.append(jsonitr)
+        return(jsonobj)
+    def post(self):
+        args = video_put_args.parse_args()
+        Name=args["Name"]
+        Age=args["Age"]
+        Mobile=args["Mobile"]
         todo=Todo(Name=Name,Age=Age,Mobile=Mobile)
         db.session.add(todo)
         db.session.commit()
-    allTodo=Todo.query.all()
-    return render_template('index.html',allTodo=allTodo)
+        return {"Name" :Name,"Age":Age,"Mobile":Mobile}
 
-
-
-@app.route('/update/<int:sno>',methods=['GET','POST'])
-def update(sno):
-    if request.method=="POST":
-        Name=request.form['Name']
-        Age=request.form['Age']
-        Mobile=request.form['Mobile']
+class EditTable(Resource):
+    def put(self,sno):
         todo = Todo.query.filter_by(sno=sno).first()
+        if not todo:
+            return{"Message":"Data Doesnt exist"}
+        args = video_put_args.parse_args()
+        Name=args['Name']
+        Age=args['Age']
+        Mobile=args['Mobile']
+       
         todo.Name=Name
         todo.Age=Age
         todo.Mobile=Mobile
         db.session.commit()
-        return redirect("/")
-    todo = Todo.query.filter_by(sno=sno).first()
-    return render_template('update.html',todo=todo)
-    
+        return{"Message":"Updated Successfully"}
 
-@app.route('/delete/<int:sno>')
-def delete(sno):
-    todo = Todo.query.filter_by(sno=sno).first()
-    db.session.delete(todo)
-    db.session.commit()
-    return redirect("/")
-if __name__=='__main__':
+    def delete(self,sno):
+        todo = Todo.query.filter_by(sno=sno).first()
+        if not todo:
+            return{"Message":"Data Doesnt exist"}
+        db.session.delete(todo)
+        db.session.commit()
+        return{"Message":"Deleted Successfull"}
+
+api.add_resource(GetTable,"/info")
+api.add_resource(EditTable,"/edit/<int:sno>")
+
+if __name__=="__main__":
     app.run(debug=True)
