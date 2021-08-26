6ff2997e7f300f60fcc98adbe85ab5f71fa85c35
Shreyas Torgalmath <37019043+shreyas6652@users.noreply.github.com>
2021-08-26 08:30:25 +0530

    Updated

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask,jsonify
 from flask_restful import Api,Resource,fields, marshal_with,reqparse
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 app=Flask(__name__)
 api=Api(app)
+CORS(app,resources=r'/api/*')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
@@ -20,10 +22,10 @@ resource_fields={
     'Age':fields.Integer,
     'Mobile':fields.String
 }
-video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("Name", type=str, required=True)
-video_put_args.add_argument("Age", type=str, required=True)
-video_put_args.add_argument("Mobile", type=str,  required=True)
+info_put_args = reqparse.RequestParser()
+info_put_args.add_argument("Name", type=str, required=True)
+info_put_args.add_argument("Age", type=str, required=True)
+info_put_args.add_argument("Mobile", type=str,  required=True)
 
 class GetTable(Resource):
     @marshal_with(resource_fields)
@@ -40,7 +42,7 @@ class GetTable(Resource):
             jsonobj.append(jsonitr)
         return(jsonobj)
     def post(self):
-        args = video_put_args.parse_args()
+        args = info_put_args.parse_args()
         Name=args["Name"]
         Age=args["Age"]
         Mobile=args["Mobile"]
@@ -54,7 +56,7 @@ class EditTable(Resource):
         todo = Todo.query.filter_by(sno=sno).first()
         if not todo:
             return{"Message":"Data Doesnt exist"}
-        args = video_put_args.parse_args()
+        args = info_put_args.parse_args()
         Name=args['Name']
         Age=args['Age']
         Mobile=args['Mobile']
@@ -73,8 +75,8 @@ class EditTable(Resource):
         db.session.commit()
         return{"Message":"Deleted Successfull"}
 
-api.add_resource(GetTable,"/info")
-api.add_resource(EditTable,"/edit/<int:sno>")
+api.add_resource(GetTable,"/api/info")
+api.add_resource(EditTable,"/api/edit/<int:sno>")
 
 if __name__=="__main__":
     app.run(debug=True)
